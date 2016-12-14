effa53c6db2bc8750861a1e4e675cbd2f9959f6f
S.tailakov <tailakovs88@gmail.com>
2016-12-14 22:36:09 +0300

    test

--- test/level07/lesson09/task01/Solution.java
+++ test/level07/lesson09/task01/Solution.java
@@ -13,7 +13,7 @@ import java.util.List;
 2. Метод printList должен выводить на экран все элементы списка  с новой строки.
 3. Используя метод printList выведи эти три списка на экран. Сначала тот, который для x%3, потом тот, который для x%2, потом последний.
 */
-
+//Ghbdtn
 public class Solution
 {
     public static void main(String[] args) throws Exception
