52bf400791b743d428e739b07fc1f3c82218b506
Тайлаков С.А <D:\MAIL>
2016-12-20 18:38:03 +0300

    20.12.16

---
+++ test/level24/lesson02/home01/SelfInterfaceMarkerImpl.java
@@ -0,0 +1,10 @@
+package com.javarush.test.level24.lesson02.home01;
+
+
+public class SelfInterfaceMarkerImpl implements SelfInterfaceMarker
+{
+
+    public void move(){}
+
+    public void print(){}
+}
