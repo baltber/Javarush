2b32b87b7fd36a80f8532fb25ad1b449a5433983
Тайлаков С.А <D:\MAIL>
2016-12-21 19:40:09 +0300

    21.12.16

---
+++ test/level24/lesson14/big01/Ball.java
@@ -0,0 +1,22 @@
+package com.javarush.test.level24.lesson14.big01;
+
+
+public class Ball extends BaseObject
+{
+    public Ball(double x, double y, double radius)
+    {
+        super(x, y, radius);
+    }
+
+    @Override
+    public void draw(Canvas canvas)
+    {
+
+    }
+
+    @Override
+    public void move()
+    {
+
+    }
+}
---
+++ test/level24/lesson14/big01/Brick.java
@@ -0,0 +1,22 @@
+package com.javarush.test.level24.lesson14.big01;
+
+public class Brick extends BaseObject
+{
+    public Brick(double x, double y, double radius)
+    {
+        super(x, y, radius);
+    }
+
+    @Override
+    public void draw(Canvas canvas)
+    {
+
+    }
+
+
+    @Override
+    public void move()
+    {
+
+    }
+}
